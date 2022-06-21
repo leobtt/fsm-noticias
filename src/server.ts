9575b1ab7fbf3cc8d4066b6b708dc69e4eac3709
leobtt <leonarbat8@gmail.com>
2022-06-20 23:46:47 -0300

    add: checking if the user was created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
+import { Users } from './models/user'
+
 const app = express()
 const port = process.env.PORT || 3000
 const mongo = process.env.MONGO || 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT&authSource=admin'
@@ -16,4 +18,18 @@ mongoose
   .connect(mongo)
   .then(() => {
     app.listen(port, () => console.log(`listening on port: ${port}`))
+  })
+
+Users.count({ username: 'leobtt' })
+  .then(total => {
+    console.log(total)
+    if (total === 0) {
+      const user = new Users({
+        username: 'leobtt',
+        password: 'adb123'
+      })
+      user.save(() => console.log('user created'))
+    } else {
+      console.log('user created skipped')
+    }
   })
