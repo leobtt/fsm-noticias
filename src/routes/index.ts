339b7393f0fe211f1a7340674a9e1ea8e27d5ef2
leobtt <leonarbat8@gmail.com>
2022-06-25 00:08:25 -0300

    add: user verification

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,24 @@
 import express from 'express'
+import { Users } from '../models/user'
 
 const router = express.Router()
 
 router.get('/login', (req, res) => {
   return res.render('login')
 })
+
+router.post('/login', async (req, res) => {
+  const user = await Users.findOne({ username: req.body.username })
+  const isValid = await user.checkingPassword(req.body.password)
+
+  if (isValid) {
+    req.session.user = user
+    return res.redirect('/restrito/noticias')
+  } else {
+    return res.redirect('/login')
+  }
+})
+
+
 
 export { router }
